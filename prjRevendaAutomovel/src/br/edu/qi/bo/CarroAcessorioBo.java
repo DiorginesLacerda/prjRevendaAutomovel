2f28854e12f28cac0ac0f1126a94fa4a383a8938
Diorgines Lacerda <diorgines.lacerda@hotmail.com>
2017-11-16 11:46:05 -0200

    Update class CarroAcessorioBo

--- prjRevendaAutomovel/src/br/edu/qi/bo/CarroAcessorioBo.java
+++ prjRevendaAutomovel/src/br/edu/qi/bo/CarroAcessorioBo.java
@@ -5,6 +5,7 @@
  */
 package br.edu.qi.bo;
 
+import br.edu.qi.dao.AcessorioDao;
 import br.edu.qi.dao.CarroAcessorioDao;
 import br.edu.qi.dao.GenericDao;
 import br.edu.qi.model.CarroAcessorio;
@@ -19,5 +20,36 @@ public class CarroAcessorioBo extends BaseBo<CarroAcessorio>{
     public CarroAcessorioBo(GenericDao<CarroAcessorio, Serializable> dao) throws Exception {
         super(CarroAcessorioDao.getInstance());
     }
+
+    @Override
+    public void delete(CarroAcessorio t) throws Exception {
+        CarroAcessorio carroA = dao.findByCod(t.getIdCarroAcessorio());
+        if(carroA!=null){
+            if(carroA.getIdCarroAcessorio().equals(t.getIdCarroAcessorio())){
+                AcessorioBo acessorioBo = new AcessorioBo(AcessorioDao.getInstance());
+                acessorioBo.removeCarroAcessorio(carroA);
+                super.delete(t);
+                return;
+            }
+        }
+        throw new Exception("Impossível Remover! Relação Carro Acessório não encontrada na base de dados");
+    }
+
+    @Override
+    public void save(CarroAcessorio t) throws Exception {
+        //verifica se o registro já existe
+        CarroAcessorio carroA = dao.findByCod(t.getIdCarroAcessorio());
+        if(carroA!= null){
+            if(carroA.getIdCarroAcessorio().equals(t.getIdCarroAcessorio()))
+                throw new Exception("Relação Carro Acessório já existente");
+        }
+        AcessorioBo acessorioBo = new AcessorioBo(AcessorioDao.getInstance());
+        //Salva o Carro acessório no acessório
+        acessorioBo.addCarroAcessorio(t);
+        //Salva a relação carroAcessório
+        super.save(t); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
     
 }
