60c9c646a437ec017edd4d7ecc12be8ae9f0c54d
Diorgines Lacerda <diorgines.lacerda@hotmail.com>
2017-11-23 14:33:41 -0200

    created  Dao Classes

---
+++ prjConcessionaria/src/br/edu/qi/dao/ModeloDao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.qi.dao;
+
+import br.edu.qi.model.Modelo;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Diorgines
+ */
+public class ModeloDao extends GenericDao<Modelo, Serializable> {
+    
+    public ModeloDao(Modelo entity) {
+        super(entity);
+    }
+    
+}
